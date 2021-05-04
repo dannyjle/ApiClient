6fd5b768ab17aee4813294bae6dc57e53b5d4beb
Danny Le <le.dannyj@gmail.com>
2021-05-04 16:01:26 -0400

    Breweries

--- Program.cs
+++ Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using ConsoleTables;
 
 namespace ApiClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Brewery
     {
-      Console.WriteLine("Welcome to C#");
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("brewery_type")]
+        public string BreweryType { get; set; }
+        [JsonPropertyName("city")]
+        public string City { get; set; }
+        [JsonPropertyName("state")]
+        public string State { get; set; }
+        [JsonPropertyName("phone")]
+        public string Phone { get; set; }
+
+
     }
-  }
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var responseAsStream = await client.GetStreamAsync("https://api.openbrewerydb.org/breweries");
+            var breweries = await JsonSerializer.DeserializeAsync<List<Brewery>>(responseAsStream);
+
+            var table = new ConsoleTable("Brewery", "Type", "City", "State", "Phone");
+
+            foreach (var brewery in breweries)
+            {
+                table.AddRow(brewery.Name, brewery.BreweryType, brewery.City, brewery.State, brewery.Phone);
+            }
+
+            table.Write();
+
+        }
+    }
 }
